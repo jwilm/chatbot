4d9885a62de50f17983424f8420277fd5e1b0a07
Joe Wilm <jdwilm@gmail.com>
2015-05-25 06:46:09 -0700

    Add SlackMsg type for better error handling
    There was previously a bunch of manual JSON unwrapping in the slack
    adapter. The SlackMsg type was added to ease handling of incoming
    messages. It implements the Decodable trait for better error handling
    and ease of use.

--- src/adapter/slack.rs
+++ src/adapter/slack.rs
@@ -9,6 +9,10 @@ use std::thread;
 
 use rustc_serialize::json::Json;
 use rustc_serialize::json;
+use rustc_serialize::json::DecoderError::MissingFieldError;
+use rustc_serialize::Decodable;
+use rustc_serialize::Decoder;
+
 use slack::Message;
 
 use message::AdapterMsg;
@@ -32,34 +36,109 @@ struct MyHandler {
   tx_adapter: Sender<AdapterMsg>
 }
 
+/// Data for a SlackMsg::Message
+#[allow(dead_code)]
+struct MessageData {
+    text: String,
+    channel: String,
+    user: String,
+    ts: String,
+    team: String
+}
+
+macro_rules! str_accessor {
+    ($s:ident) => {
+        pub fn $s(&self) -> &str {
+            self.$s.as_ref()
+        }
+    }
+}
+
+#[allow(dead_code)]
+impl MessageData {
+    str_accessor!(text);
+    str_accessor!(channel);
+    str_accessor!(user);
+    str_accessor!(ts);
+    str_accessor!(team);
+}
+
+/// Incoming slack messages on the websocket api
+enum SlackMsg {
+    /// A message was sent to a channel
+    Message(MessageData),
+
+    /// Some other type of message arrived.
+    /// [The list](https://api.slack.com/rtm) is quite extensive, and only the
+    /// messages the adapter is concerned with are enumerated here.
+    Other
+}
+
+impl Decodable for SlackMsg {
+    fn decode<D: Decoder>(d: &mut D) -> Result<SlackMsg, D::Error> {
+        d.read_struct("root", 0, |root| {
+            let msg_type = try!(root.read_struct_field("type", 0, |f| f.read_str() ));
+
+            match msg_type.as_ref() {
+                "message" => {
+                    Ok(SlackMsg::Message(MessageData {
+                        text: try!(root.read_struct_field("text", 0, |f| f.read_str())),
+                        channel: try!(root.read_struct_field("channel", 0, |f| f.read_str())),
+                        user: try!(root.read_struct_field("user", 0, |f| f.read_str())),
+                        ts: try!(root.read_struct_field("ts", 0, |f| f.read_str())),
+                        team: try!(root.read_struct_field("team", 0, |f| f.read_str())),
+                    }))
+                },
+                _ => {
+                    Ok(SlackMsg::Other)
+                }
+            }
+        })
+    }
+}
+
+fn string_to_slack_msg(raw: &str) -> Result<SlackMsg, json::DecoderError> {
+    // Some messages arriving from the slack client don't have a type. So far I've only
+    // witnessed confirmation messages arriving in this fashion. Since they go through the same
+    // pipeline as content messages, the decoder should be able to handle them.
+    match json::decode::<SlackMsg>(raw) {
+        Ok(value) => Ok(value),
+        Err(e) => {
+            match e {
+                MissingFieldError(field) => {
+                    match field.as_ref() {
+                        "type" => Ok(SlackMsg::Other),
+                        _ => Err(MissingFieldError(field))
+                    }
+                },
+                _ => Err(e)
+            }
+        }
+    }
+}
+
 #[allow(unused_variables)]
 impl slack::MessageHandler for MyHandler {
-    fn on_receive(&mut self, cli: &mut slack::RtmClient, json_str: &str) {
-        println!("Received[{}]: {}", self.count, json_str.to_string());
+    fn on_receive(&mut self, cli: &mut slack::RtmClient, raw: &str) {
+        println!("Received[{}]: {}", self.count, raw.to_string());
         self.count = self.count + 1;
-        let json_thing = Json::from_str(json_str).unwrap();
-        let json = json_thing.as_object().unwrap();
 
-        let msg_type = match json.get("type") {
-            Some(json_str) => json_str.as_string().unwrap(),
-            None => return
-        };
+        match string_to_slack_msg(raw) {
+            Ok(slack_msg) => {
+                match slack_msg {
+                    SlackMsg::Message(msg) => {
+                        let incoming = IncomingMessage::new("SlackAdapter".to_owned(), None,
+                            Some(msg.channel().to_owned()), Some(msg.user().to_owned()),
+                            msg.text().to_owned(), self.tx_adapter.clone());
 
-        match msg_type.as_ref() {
-            "message" => {
-                // TODO custom types for incoming messages.. unwrapunwrapunwrapunwrap
-                let channel = json.get("channel").unwrap().as_string().unwrap();
-                let user = json.get("user").unwrap().as_string().unwrap();
-                let payload = json.get("text").unwrap().as_string().unwrap();
-
-                let msg = IncomingMessage::new("SlackAdapter".to_owned(), None,
-                    Some(channel.to_owned()), Some(user.to_owned()), payload.to_owned(),
-                    self.tx_adapter.clone());
-
-                self.tx_bot.send(msg).unwrap();
+                        self.tx_bot.send(incoming).ok().expect("Bot unable to process messages");
+                    },
+                    _ => ()
+                }
             },
-            _ => {
-                return ();
+            Err(e) => {
+                println!("error decoding slack message: {:?}", e);
+                println!("please consider reporting this to jwilm/chatbot as it is probably a bug");
             }
         }
     }
@@ -138,3 +217,50 @@ impl ChatAdapter for SlackAdapter {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use adapter::slack::SlackMsg;
+    use adapter::slack::string_to_slack_msg;
+
+    #[test]
+    fn decode_message() {
+        let raw = "{\"type\":\"message\", \
+                    \"channel\":\"D04UYUAMW\", \
+                    \"user\":\"U02ALMR84\", \
+                    \"text\":\"ping\", \
+                    \"ts\":\"1432563914.000007\", \
+                    \"team\":\"T02ALMR82\"}";
+
+        let slack_msg = string_to_slack_msg(raw).unwrap();
+
+        match slack_msg {
+            SlackMsg::Message(data) => {
+                assert_eq!(data.text(), "ping");
+                assert_eq!(data.channel(), "D04UYUAMW");
+                assert_eq!(data.user(), "U02ALMR84");
+                assert_eq!(data.ts(), "1432563914.000007");
+                assert_eq!(data.team(), "T02ALMR82");
+            },
+            _ => panic!("Expected SlackMsg::Message")
+        }
+    }
+    #[test]
+    fn decode_confirmation_message() {
+        let raw = r#"{"ok":true,"reply_to":0,"ts":"1432566639.000014","text":"pong"}"#;
+
+        match string_to_slack_msg(raw).unwrap() {
+            SlackMsg::Other => (),
+            _ => panic!("expected SlackMsg::Other")
+        }
+    }
+
+    #[test]
+    fn decode_unexpected_type() {
+        let raw = r#"{"type":"not_a_slack_msg_type"}"#;
+
+        match string_to_slack_msg(raw).unwrap() {
+            SlackMsg::Other => (),
+            _ => panic!("expected SlackMsg::Other")
+        }
+    }
+}
